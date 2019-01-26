e2494b9e1135e14104be8b8c2510e7587d66a2d5
stephen-hoaxing <neville.rockbottom@gmail.com>
2019-01-26 21:26:58 +0100

    Configuration

---
+++ src/main/java/com/nidal/neo4jhonved/Neo4jHonvedApplication.java
@@ -0,0 +1,14 @@
+package com.nidal.neo4jhonved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jHonvedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jHonvedApplication.class, args);
+	}
+
+}
+
